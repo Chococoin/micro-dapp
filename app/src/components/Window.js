36ca3b2e5959242bba15532c7e630bdb312f5d3a
Germán Lugo <german.lugo@bitnibs.com>
2018-09-08 00:10:06 +0200

    Add story.

---
+++ app/src/components/Window.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Window extends Component {
+	render(){
+		return(
+			<div>
+				"Hola mundo"
+			</div>
+		);
+	};
+}
+
+export default Window;
--- app/src/stories/index.js
+++ app/src/stories/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Button } from '@storybook/react/demo';
+import App from '../App';
+import Window from '../components/Window';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('My Component', module).add('Square', () => <App />);
+
+storiesOf('New Component', module)
+  .add('New square', () => <Window />);
+
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
