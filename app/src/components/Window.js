fb44703f706c14e856397172244b83ba4c66e9c7
Germán Lugo <german.lugo@bitnibs.com>
2018-09-08 01:08:20 +0200

    Last commit before bed.

--- app/src/components/Window.js
+++ app/src/components/Window.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-
+import '../styles/Window.css';
 
 class Window extends Component {
-	render(){
-		return(
-			<div>
-				"Hola mundo"
-			</div>
-		);
-	};
+    constructor(){
+      super()
+      this.state = {
+        text: "Hola mundo",
+      };
+    }
+
+    componentWillMount(){
+      this.setState({text: "Ciao mundo!"});
+    }
+
+
+    render(){
+        return(
+            <div className="inData">
+                "{this.state.text}"
+            </div>
+        );
+    };
 }
 
 export default Window;
